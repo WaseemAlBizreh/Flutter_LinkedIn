75f23306efa0768afefa58d2b28c37ba078851bf
WaseemAlbizreh <waseem.bez@gmail.com>
2023-07-18 21:55:56 +0300

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_linkedin/screens/date_picker_validation.dart';
-import 'package:flutter_linkedin/theme/app_color.dart';
 import 'package:flutter_linkedin/theme/theme_data.dart';
 
 void main() {
@@ -10,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
